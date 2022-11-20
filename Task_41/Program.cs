9f9ec0ffae75ad14c99e240a26f6ad3ab27727cf
Klim <naiberd669@bk.ru>
2022-11-20 21:03:01 +0300

    Task_41 done

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 4
+*/
+
+int NatureNumber(string ending)
+{
+    bool check = false;
+    int count = 0;
+    int number = 0;
+    string? symbol = String.Empty;
+    while (check == false)
+    {
+        symbol = Console.ReadLine();
+        if (symbol == ending)
+            check = true;
+        else
+        {
+            number = Convert.ToInt32(symbol);
+            if (number > 0) count++;
+            Console.WriteLine($"Count={count}");
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введи числа для подсчета");
+Console.WriteLine("Для завершения ввода чисел, введи q");
+string end = "q";
+int count = NatureNumber(end);
+Console.WriteLine($"Количество чисел больше нуля: {count}");
